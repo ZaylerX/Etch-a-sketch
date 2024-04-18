5ea714e03850ac15a06c06e651d9ac1571519437
ZaylerX <ZaylerX@live.it>
2024-04-19 00:18:26 +0800

    Creata la griglia, da capire js
    Creata la funzione per generare colore casuale, non capisco perché il mouseover non funziona

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const container = document.querySelector(".container");
-let gridSquare;
+let gridCell;
 let rowContainer;
 
 function createRows () {
@@ -7,18 +7,36 @@ function createRows () {
         rowContainer = document.createElement("div");
         rowContainer.classList.add("rowContainer");
         container.appendChild(rowContainer);
+        createCell();
+        }
     }
-}
 
-function createDivs() {
-    for (i = 0; i < 16; i++) {
-    gridSquare = document.createElement("div");
-    gridSquare.classList.add("gridSquare");
-    gridSquare.textContent = "Aaa";
-    rowContainer.appendChild(gridSquare);
-    }    
-}
+    function createCell() {
+        for (j = 0; j < 16; j++) {
+            gridCell = document.createElement("div");
+            gridCell.classList.add("gridSquare");
+            rowContainer.appendChild(gridCell);
+            }    
+    }
 
 createRows();
-rowContainer.forEach(createDivs());
+
+
+let rdmColor;
+function getRandomColor() {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)]
+    }  
+rdmColor = color; 
+} 
+
+getRandomColor();
+
+gridCell.addEventListener("mouseover", () => {
+    gridCell.style.backgroundColor = rdmColor;
+}); 
+
+
 
